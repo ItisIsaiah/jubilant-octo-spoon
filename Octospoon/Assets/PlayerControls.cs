f546c07f563c73adc63994eb62da92828dc12c47
ZayX731 <zayfootballplayer@gmail.com>
2021-03-25 17:54:17 -0400

    Controller Implementation
    Fixed the jump code so that you actually jump off the ground but need to add parameters so that we dont have the classic infinite jump. made the groundpoints to begin this process in hopes to check if the player is on the ground so we can jump. Added some stuff for the player to be able to use the controller to walk around and jump. The system made for the controller is completely different from that of the keyboard because simply I needed it to be like that.  Also I want a way to implement the fade shader but havent really found a good way to do that yet.

---
+++ Octospoon/Assets/PlayerControls.cs
@@ -0,0 +1,161 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""2fa541c6-5df0-445b-96d6-814e53d96a9b"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""543b7b06-f150-46a3-8fa3-dce114079667"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""e988b131-9162-4dfb-97dc-8409f9d8b91b"",
+                    ""expectedControlType"": ""Stick"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""a6f2409d-69f2-4984-b6f1-f0ba8d91ac04"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0d715c60-020c-47dd-8ae8-909bb4d399ec"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Jump;
+    private readonly InputAction m_Gameplay_Move;
+    public struct GameplayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GameplayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump => m_Wrapper.m_Gameplay_Jump;
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Jump.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnJump(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
--- Octospoon/Assets/Scripts/playerMove.cs
+++ Octospoon/Assets/Scripts/playerMove.cs
@@ -1,21 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMove : MonoBehaviour
 {
+    //Movement
     float horizontal;
-    float jumpForce;
+    Vector2 moveC;
+
+    //
+    public Material fade;
+
+    //speeds
+    public float jumpForce=20f;
     public float moveSpeed = 20f;
+    public float controllerMoveSpeed = 20f;
+
+    //logic
     Rigidbody2D rb;
     bool facingRight = true;
 
+    public Transform[] groundpoints;
+    PlayerControls controls;
+
     public Animator anim;
     // Start is called before the first frame update
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Gameplay.Jump.performed += ctx=> jump();
+
+        controls.Gameplay.Move.performed += ctx => moveC = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => moveC = Vector2.zero;
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         facingRight = true;
+        
         
     }
 
@@ -26,11 +50,16 @@ public class playerMove : MonoBehaviour
 
 
 
-        
+
+        Vector2 m = new Vector2(moveC.x*controllerMoveSpeed, moveC.y) * Time.deltaTime;
+        transform.Translate(m, Space.World);
 
 
-        if (Input.GetKey(KeyCode.Space))
+        //
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Same");
             jump();
         }
 
@@ -39,10 +68,21 @@ public class playerMove : MonoBehaviour
             flip();
             Debug.Log("me too!");
         }
+
+
         HandleAnimation();
         move(horizontal);
     }
+   
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
 
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
     void move(float horizontal)
     {
         
@@ -77,6 +117,11 @@ public class playerMove : MonoBehaviour
     void HandleAnimation()
     {
         anim.SetFloat("speed", Mathf.Abs(horizontal));
+        anim.SetFloat("speed2", Mathf.Abs(moveC.x));
+    }
+   void OnDrawGizmos()
+    {
+        
     }
 }
 
